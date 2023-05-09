639f629e1c8adf5f91f7ccee7e79af1e0d71d753
Joshua Mwesigwa <joshuamwesigwa256@gmail.com>
2023-05-09 10:55:22 +0300

    The initialized array and the string literals

--- anagram_finder/anagram_finder.c
+++ anagram_finder/anagram_finder.c
@@ -7,5 +7,9 @@
 
 int main()
 {
-	
+	int counter1[] = {'0', '0', '0', '0'};
+	int counter2[] = {'0', '0', '0', '0'};
+
+	char s1[] = "dbb cccccaacb cdbababdcdcdab dcdad";
+	char s2[] = "bbbcc bdddccccad cdbbaaacaccdabdd";	
 }
