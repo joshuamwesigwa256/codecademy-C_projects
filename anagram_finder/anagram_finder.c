8cb9fb4ac5e866912d7f6efd8f5f0ecc2172a9b4
Joshua Mwesigwa <joshuamwesigwa256@gmail.com>
2023-05-09 15:44:40 +0300

    Finished programme

--- anagram_finder/anagram_finder.c
+++ anagram_finder/anagram_finder.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 #include <string.h>
-
-/**
- *
- */
-
-int main(){
-	/initialization for counters a, b, c, d
+int main() 
+{
+  //initialization for counters a, b, c, d
   int counter1[] = {0, 0, 0, 0}; 
   int counter2[] = {0, 0, 0, 0};
 
@@ -33,22 +29,34 @@ int main(){
   }
 
 //counting the characters in string 2
-  for (int j = 0; j < strlen(s2); j++){
-    if (s2[j] == 'a'){
+  for (int i = 0; i < strlen(s2); i++){
+    if (s2[i] == 'a'){
       counter2[0]++;
     }
-    if (s2[j] == 'b'){
+    if (s2[i] == 'b'){
       counter2[1]++;
     }
-    if (s2[j] == 'c'){
+    if (s2[i] == 'c'){
       counter2[2]++;
     }
-    if (s2[j] == 'd'){
+    if (s2[i] == 'd'){
       counter2[3]++;
     }
-    if (s2[j] == ' '){
+    if (s2[i] == ' '){
       continue;
     }
   }
+  int flag = 0;
+  for (int i = 0; i < 4; i++){
+    if (counter1[i] != counter2[i]){
+      flag = 1;
+      break;
+    }
+  }
+  if (flag = 0)
+    printf("Anagrams\n");
+  if (flag = 1)
+  printf("Not Anagrams\n");
+
   
 }
